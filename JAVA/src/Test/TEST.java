8d61d7d945fccd96756e188dc5da3110060e889e
Taesung <202145011@itc.ac.kr>
2022-05-28 19:31:06 +0900

    Update TEST.java

--- JAVA/src/Test/TEST.java
+++ JAVA/src/Test/TEST.java
@@ -1,12 +1,17 @@
 package Test;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Desktop;
+import java.awt.Dimension;
+
 import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import java.awt.SystemColor;
 import javax.swing.UIManager;
@@ -60,7 +65,7 @@ public class TEST extends JFrame implements MouseListener, MouseMotionListener, 
 		setIconImage(Toolkit.getDefaultToolkit().getImage(TEST.class.getResource("/Image/IconImg.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 865, 574);
-		setLocation(700,200);
+		setLocation(400,200);
 		setResizable(false);
 		setUndecorated(true);
 		contentPane = new GraPanel();
@@ -140,6 +145,129 @@ public class TEST extends JFrame implements MouseListener, MouseMotionListener, 
 		Pane1.setBackground(Color.GREEN);
 		Pane1.setBounds(0, 0, 694, 545);
 		MainPane.add(Pane1);
+		
+		// 여기서부터 '병원 리스트' Panel 단
+/*		ImageIcon backgroundicon = new ImageIcon(TEST.class.getResource("/image/listback.png"));
+		JLabel BackgroundimgLabel = new JLabel();
+		BackgroundimgLabel.setIcon(backgroundicon);
+		BackgroundimgLabel.setBounds(0,0,694,545);
+		BackgroundimgLabel.setHorizontalAlignment(JLabel.CENTER);
+		Pane1.add(BackgroundimgLabel);
+		//BackgroundimgLabel.setComponentZOrder(BackgroundimgLabel, 1);
+		*/
+		
+		
+		ImageIcon icon = new ImageIcon(TEST.class.getResource("/image/listpic.png"));
+		JLabel ListimgLabel = new JLabel();
+		ListimgLabel.setIcon(icon);
+		ListimgLabel.setBounds(230,20,50,60);
+		ListimgLabel.setHorizontalAlignment(JLabel.CENTER);
+		Pane1.add(ListimgLabel);
+		
+		JLabel listlabel0 = new JLabel("병원 리스트");
+		listlabel0.setFont(new Font("맑은고딕", Font.BOLD, 30));
+		listlabel0.setBounds(287, 30, 200, 30);
+		Pane1.add(listlabel0);
+		
+		
+		
+		// 리스트 첫번째 병원
+		JPanel Panel1 = new JPanel();
+		Panel1.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		Panel1.setBounds(147, 100, 400, 50);
+		
+		JLabel listlabel1 = new JLabel("병원1");
+		listlabel1.setBorder(BorderFactory.createEmptyBorder(0,20,0,10));
+		listlabel1.setPreferredSize(new Dimension(300,50));
+		Panel1.add(listlabel1);
+		
+		JButton listbtn1 = new JButton("병원쓰");
+		listbtn1.setAlignmentX(RIGHT_ALIGNMENT);
+		Panel1.add(listbtn1);
+		
+		// 리스트 두번째 병원
+		JPanel Panel2 = new JPanel();
+		Panel2.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		Panel2.setBounds(147, 170, 400, 50);
+		
+		JLabel listlabel2 = new JLabel("병원2");
+		listlabel2.setBorder(BorderFactory.createEmptyBorder(0,20,0,10));
+		listlabel2.setPreferredSize(new Dimension(300,50));
+		Panel2.add(listlabel2);
+		
+		JButton listbtn2 = new JButton("병원쓰");
+		listbtn2.setAlignmentX(RIGHT_ALIGNMENT);
+		Panel2.add(listbtn2);
+		
+		// 리스트 세번째 병원
+		JPanel Panel3 = new JPanel();
+		Panel3.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		Panel3.setBounds(147, 240, 400, 50);
+		
+		JLabel listlabel3 = new JLabel("병원3");
+		listlabel3.setBorder(BorderFactory.createEmptyBorder(0,20,0,10));
+		listlabel3.setPreferredSize(new Dimension(300,50));
+		Panel3.add(listlabel3);
+		
+		JButton listbtn3 = new JButton("병원쓰");
+		listbtn3.setAlignmentX(RIGHT_ALIGNMENT);
+		Panel3.add(listbtn3);
+		
+		// 리스트 세번째 병원
+		JPanel Panel4 = new JPanel();
+		Panel4.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		Panel4.setBounds(147, 310, 400, 50);
+		
+		JLabel listlabel4 = new JLabel("병원4");
+		listlabel4.setBorder(BorderFactory.createEmptyBorder(0,20,0,10));
+		listlabel4.setPreferredSize(new Dimension(300,50));
+		Panel4.add(listlabel4);
+		
+		JButton listbtn4 = new JButton("병원쓰");
+		listbtn4.setAlignmentX(RIGHT_ALIGNMENT);
+		Panel4.add(listbtn4);
+		
+		// 리스트 다섯번째 병원
+		JPanel Panel5 = new JPanel();
+		Panel5.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		Panel5.setBounds(147, 380, 400, 50);
+		
+		JLabel listlabel5 = new JLabel("병원5");
+		listlabel5.setBorder(BorderFactory.createEmptyBorder(0,20,0,10));
+		listlabel5.setPreferredSize(new Dimension(300,50));
+		Panel5.add(listlabel5);
+		
+		JButton listbtn5 = new JButton("병원쓰");
+		listbtn5.setAlignmentX(RIGHT_ALIGNMENT);
+		Panel5.add(listbtn5);
+		
+		// 리스트 여섯번째 병원
+		JPanel Panel6 = new JPanel();
+		Panel6.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		Panel6.setBounds(147, 450, 400, 50);
+		
+		JLabel listlabel6 = new JLabel("병원6");
+		listlabel6.setBorder(BorderFactory.createEmptyBorder(0,20,0,10));
+		listlabel6.setPreferredSize(new Dimension(300,50));
+		Panel6.add(listlabel6);
+		
+		JButton listbtn6 = new JButton("병원쓰");
+		listbtn6.setAlignmentX(RIGHT_ALIGNMENT);
+		Panel6.add(listbtn6);
+		
+		
+		
+		
+		
+		Pane1.add(Panel1);
+		Pane1.add(Panel2);
+		Pane1.add(Panel3);
+		Pane1.add(Panel4);
+		Pane1.add(Panel5);
+		Pane1.add(Panel6);
+		
+		// 여기까지 '병원 리스트' 단
+		
 		
 		Pane2 = new JPanel();
 		Pane2.setLayout(null);
